173c362ad7523c2db7c547d65a420a880300298e
L0Z1K <qortmddbs731@naver.com>
2022-08-15 22:28:02 +0900

    feat: Add PoW

--- README.md
+++ README.md
@@ -4,5 +4,25 @@ A simplified blockchain implementation in Golang (Jeiwan ver.)
 ### Try it Out
 
 ```bash
-$ go run main.go
+❯ go run .
+Mining the block containing "Genesis Block"
+00000292c9433165ad07a6243b491f405aecb4588f82faf109b5759aa1f1f793
+
+Mining the block containing "Send 1 BTC to Ivan"
+0000071eb55bd044f87c6bdd932c0694d2179611dce0a0fb1f7fb3d4457f102c
+
+Mining the block containing "Send 2 more BTC to Ivan"
+000007dec8e227bbf9e1459e3ab325d306106e31da8f285b7c8082fb6797985c
+
+Prev. hash: 
+Data: Genesis Block
+Hash: 00000292c9433165ad07a6243b491f405aecb4588f82faf109b5759aa1f1f793
+
+Prev. hash: 00000292c9433165ad07a6243b491f405aecb4588f82faf109b5759aa1f1f793
+Data: Send 1 BTC to Ivan
+Hash: 0000071eb55bd044f87c6bdd932c0694d2179611dce0a0fb1f7fb3d4457f102c
+
+Prev. hash: 0000071eb55bd044f87c6bdd932c0694d2179611dce0a0fb1f7fb3d4457f102c
+Data: Send 2 more BTC to Ivan
+Hash: 000007dec8e227bbf9e1459e3ab325d306106e31da8f285b7c8082fb6797985c
 ```
--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
-	"strconv"
 	"time"
 )
 
@@ -13,19 +10,24 @@ type Block struct {
 	Data          []byte // actual valuable info
 	PrevBlockHash []byte // hash of previous block
 	Hash          []byte // hash of block
+	Nonce         int
 }
 
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
+// func (b *Block) SetHash() {
+// 	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+// 	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+// 	hash := sha256.Sum256(headers)
 
-	b.Hash = hash[:]
-}
+// 	b.Hash = hash[:]
+// }
 
 func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
 	return block
 }
 
