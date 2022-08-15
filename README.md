9c6b1b556d0f0629f92ac91978674d79912e4920
Seungyun Baek <64528476+L0Z1K@users.noreply.github.com>
2022-08-15 22:41:21 +0900

    Part 2: Proof-of-Work (#2)
    * feat: Add PoW
    
    * feat: Add Validation Check
    
    * refactor: Split code from main

--- README.md
+++ README.md
@@ -4,5 +4,28 @@ A simplified blockchain implementation in Golang (Jeiwan ver.)
 ### Try it Out
 
 ```bash
-$ go run main.go
+❯ go run .
+Mining the block containing "Genesis Block"
+00000b24d04b24281f0d8616dc1e992a7ba7558bec998a171e8e511282e7b70c
+
+Mining the block containing "Send 1 BTC to Ivan"
+00000b4781887f105726db8b2ec8bc439829b0d4aa92fd0cc2543fcd90ee3c97
+
+Mining the block containing "Send 2 more BTC to Ivan"
+000005bbd5072ef46bfc89a50824b1e9390e10f55754935ba314b87169fdb3d2
+
+Prev. hash: 
+Data: Genesis Block
+Hash: 00000b24d04b24281f0d8616dc1e992a7ba7558bec998a171e8e511282e7b70c
+PoW: true
+
+Prev. hash: 00000b24d04b24281f0d8616dc1e992a7ba7558bec998a171e8e511282e7b70c
+Data: Send 1 BTC to Ivan
+Hash: 00000b4781887f105726db8b2ec8bc439829b0d4aa92fd0cc2543fcd90ee3c97
+PoW: true
+
+Prev. hash: 00000b4781887f105726db8b2ec8bc439829b0d4aa92fd0cc2543fcd90ee3c97
+Data: Send 2 more BTC to Ivan
+Hash: 000005bbd5072ef46bfc89a50824b1e9390e10f55754935ba314b87169fdb3d2
+PoW: true
 ```
--- main.go
+++ main.go
@@ -1,52 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
 	"strconv"
-	"time"
 )
-
-type Block struct {
-	Timestamp     int64  // current timestamp
-	Data          []byte // actual valuable info
-	PrevBlockHash []byte // hash of previous block
-	Hash          []byte // hash of block
-}
-
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-
-	b.Hash = hash[:]
-}
-
-func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
-	return block
-}
-
-type Blockchain struct {
-	blocks []*Block
-}
-
-func (bc *Blockchain) AddBlock(data string) {
-	prevBlock := bc.blocks[len(bc.blocks)-1]
-	newBlock := NewBlock(data, prevBlock.Hash)
-	bc.blocks = append(bc.blocks, newBlock)
-}
-
-// The first block in the chain
-func NewGenesisBlock() *Block {
-	return NewBlock("Genesis Block", []byte{})
-}
-
-func NewBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{NewGenesisBlock()}}
-}
 
 func main() {
 	bc := NewBlockchain()
@@ -57,6 +14,8 @@ func main() {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 	}
 }
