c1a3ce734831f75823d0732d5051cd3565997059
L0Z1K <qortmddbs731@naver.com>
2022-08-17 21:16:35 +0900

    feat: Serialization

--- block.go
+++ block.go
@@ -1,6 +1,10 @@
 package main
 
-import "time"
+import (
+	"bytes"
+	"encoding/gob"
+	"time"
+)
 
 type Block struct {
 	Timestamp     int64  // current timestamp
@@ -31,4 +35,28 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 // The first block in the chain
 func NewGenesisBlock() *Block {
 	return NewBlock("Genesis Block", []byte{})
+}
+
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		panic(err)
+	}
+
+	return result.Bytes()
+}
+
+func DeserializeBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		panic(err)
+	}
+
+	return &block
 }
